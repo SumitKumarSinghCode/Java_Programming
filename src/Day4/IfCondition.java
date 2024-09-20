efc759e1861b39c35e0dd36bfc6470f82fe74b55
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-09-20 19:01:22 +0530

    update

--- src/Day4/IfCondition.java
+++ src/Day4/IfCondition.java
@@ -1,5 +1,7 @@
 package Day4;
 
+import java.util.Scanner;
+
 public class IfCondition {
     public static void main(String[] args){
 
@@ -16,6 +18,17 @@ public class IfCondition {
         }
         else {
             System.out.println("Note Eligible for vote");
+        }
+
+        //Check if Number is Even or add
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter a number to check It's Even or Odd: ");
+        int number = scn.nextInt();
+        if ((number%2) == 0){
+            System.out.println("Even Number");
+        }
+        else{
+            System.out.println("Odd Number");
         }
     }
 }
