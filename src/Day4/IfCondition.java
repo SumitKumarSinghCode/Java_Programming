4e32eee3136569681c8688b7ff4ebddd7fea7034
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-09-20 18:45:53 +0530

    update

--- src/Day4/IfCondition.java
+++ src/Day4/IfCondition.java
@@ -2,10 +2,20 @@ package Day4;
 
 public class IfCondition {
     public static void main(String[] args){
-        int person_age = 25;
-        if (person_age >= 18){
+
+        //if Conditional Statement
+        int person_age1 = 19;
+        if (person_age1 >= 18){
             System.out.println("Eligible for Vote");
         }
 
+        //if else Conditional Statement
+        int person_age2 = 17;
+        if (person_age2>=18){
+            System.out.println("Eligible for vote");
+        }
+        else {
+            System.out.println("Note Eligible for vote");
+        }
     }
 }
---
+++ src/Day4/NestedIfElse.java
@@ -0,0 +1,9 @@
+package Day4;
+
+public class NestedIfElse {
+    public static void main(String[] args){
+        //nested if else Conditional Statement
+
+
+    }
+}
