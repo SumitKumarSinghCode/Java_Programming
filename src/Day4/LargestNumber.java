20881d96c9d8f1f2a25823a1be2bbf78ddf9d3ab
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-09-21 15:14:53 +0530

    update

--- src/Day4/LargestNumber.java
+++ src/Day4/LargestNumber.java
@@ -6,12 +6,22 @@ public class LargestNumber {
     public static void main(String[] args){
         //Largest of 3 numbers
         Scanner numb = new Scanner(System.in);
-        System.out.print("Enter the first number: ");
-        int n1 = numb.nextInt();
-        System.out.print("Enter the second number: ");
-        int n2 = numb.nextInt();
+        System.out.println("Enter the first number: ");
+        int a = numb.nextInt();
+        System.out.println("Enter the second number: ");
+        int b = numb.nextInt();
         System.out.println("Enter the third number: ");
-        int n3 = numb.nextInt();
-        System.out.print(n1+" "+n2+" "+n3);
+        int c = numb.nextInt();
+        System.out.println("a="+a+", b="+b+", c="+c);
+
+        if ((a>b) && (a>c)){
+            System.out.println(a+", a is greatest");
+        }
+        else if ((b>a) && (b>c)){
+            System.out.println(b+", b is greatest");
+        }
+        else{
+            System.out.println(c+", c is greatest");
+        }
     }
 }
