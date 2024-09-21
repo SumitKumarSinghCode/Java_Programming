058f330419241661271c649290b814e90b809cf3
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-09-21 17:26:00 +0530

    update

--- src/Day4/DisplayWeekNames.java
+++ src/Day4/DisplayWeekNames.java
@@ -9,19 +9,19 @@ public class DisplayWeekNames {
         int week_day = scn.nextInt();
 
         if (week_day==1){
-            System.out.println("Monday");
-        }else if(week_day==2){
-            System.out.println("Tuesday");
-        }else if(week_day==3){
-            System.out.println("Wednesday");
-        }else if(week_day==4){
-            System.out.println("Thursday");
-        }else if(week_day==5){
-            System.out.println("Friday");
-        }else if(week_day==6){
-            System.out.println("Saturday");
-        }else if(week_day==7){
             System.out.println("Sunday");
+        }else if(week_day==2){
+            System.out.println("Monday");
+        }else if(week_day==3){
+            System.out.println("Tuesday");
+        }else if(week_day==4){
+            System.out.println("Wednesday");
+        }else if(week_day==5){
+            System.out.println("Thursday");
+        }else if(week_day==6){
+            System.out.println("Friday");
+        }else if(week_day==7){
+            System.out.println("Saturday");
         }else{
             System.out.println("Enter a valid week day");
         }
