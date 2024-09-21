0c0e629459faf6152cda8afc3faf1da914a7acee
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-09-21 15:42:39 +0530

    update

--- src/Day4/NestedIfElse.java
+++ src/Day4/NestedIfElse.java
@@ -4,6 +4,19 @@ public class NestedIfElse {
     public static void main(String[] args){
         //nested if else Conditional Statement
 
+        if (false){
+            if(true) {
+                System.out.println("1");
+            }else{
+                System.out.println("2");
+            }
+        }else{
+            if(true){
+                System.out.println("A");
+            }else{
+                System.out.println("B");
+            }
+        }
 
     }
 }
