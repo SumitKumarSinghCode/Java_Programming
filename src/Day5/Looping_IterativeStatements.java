65c0af8493010bd533a5984bac114ccc243a7791
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-09-25 17:06:02 +0530

    update

---
+++ src/Day5/Looping_IterativeStatements.java
@@ -0,0 +1,27 @@
+package Day5;
+
+public class Looping_IterativeStatements {
+    public static void main(String[] args){
+        //Looping / IterativeStatements
+        //1) while loop
+        //2) do while loop
+        //3) for loop
+        //4) enhanced for loop / for each loop
+
+        // 3 things to keep in mind while using loops
+
+        // initialization (Where to Start)
+        // condition (How many times)
+        // Increment / Decrement
+
+
+        //Syntax of while loop
+        /*
+        initialization;
+        while(condition){
+           statements;
+           inc/dec;
+        }
+        */
+    }
+}
---
+++ src/Day5/WhileLoop.java
@@ -0,0 +1,13 @@
+package Day5;
+
+public class WhileLoop {
+    public static void main(String[] args){
+        //Print number from 1-10 numbers
+        int i = 1; //initialization
+        while (i<=10) //Condition
+        {
+            System.out.println(i);
+            i++; //inc
+        }
+    }
+}
