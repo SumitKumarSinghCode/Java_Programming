1613087b0ab8cdbf88abccafa624e7a46b423242
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-09-27 16:02:52 +0530

    update

--- src/Day6/Assignment1.java
+++ src/Day6/Assignment1.java
@@ -3,5 +3,11 @@ package Day6;
 public class Assignment1 {
     public static void main(String[] args){
         //Find sum of elements in an array. Example:- Input= [1,2,3,4,5]  Output= 15
+        int arr[] = {1,2,3,4,5};
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        System.out.println("Sum of all the element in an array is "+sum); //15
     }
 }
--- src/Day6/Assignment2.java
+++ src/Day6/Assignment2.java
@@ -2,7 +2,19 @@ package Day6;
 
 public class Assignment2 {
     public static void main(String[] args){
-        //Print Even or Odd numbers from an array Example:- Input= [1,2,3,4,5,6] Output= Even= 3 Odd= 3
-
+        //Print count of Even or Odd numbers from an array Example:- Input= [1,2,3,4,5,6] Output= Even= 3 Odd= 3
+        int arr[] = {1,2,3,4,5,6,7};
+        int even = 0;
+        int odd = 0;
+        for(int i=0; i<arr.length;i++){
+            if(arr[i]%2==0){
+                even++;
+            }
+            else{
+                odd++;
+            }
+        }
+        System.out.println("Even= "+even);
+        System.out.println("Odd= "+odd);
     }
 }
