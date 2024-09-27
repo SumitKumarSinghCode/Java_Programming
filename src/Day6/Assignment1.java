eb8203463e42dc62abc299c9f67e8ca6bd0270f9
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-09-27 14:39:02 +0530

    update

---
+++ src/Day6/Assignment1.java
@@ -0,0 +1,7 @@
+package Day6;
+
+public class Assignment1 {
+    public static void main(String[] args){
+        //Find sum of elements in an array. Example:- Input= [1,2,3,4,5]  Output= 15
+    }
+}
---
+++ src/Day6/Assignment2.java
@@ -0,0 +1,8 @@
+package Day6;
+
+public class Assignment2 {
+    public static void main(String[] args){
+        //Print Even or Odd numbers from an array Example:- Input= [1,2,3,4,5,6] Output= Even= 3 Odd= 3
+
+    }
+}
