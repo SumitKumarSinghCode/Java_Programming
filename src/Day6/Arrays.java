7010807d158632bda4964268a542aaa663718525
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-09-27 11:31:26 +0530

    update

---
+++ src/Day6/Arrays.java
@@ -0,0 +1,18 @@
+package Day6;
+
+public class Arrays {
+    public static void main(String[] args){
+        // Arrays
+        /*
+        Arrays:- Arrays is collection of elements of same data type(Homogenios data).
+        We can store multiple values into a single variable.
+
+        Syntax:-
+        int a[] = new int[size];
+
+        Types of Arrays:-
+        1) Single Dimensional
+        2) Two Dimensional/multi dimensionals
+         */
+    }
+}
