90865db886d5b55e377147a59859f972131b7fbd
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-10-04 13:33:55 +0530

    update

--- src/Day7/Assignment2.java
+++ src/Day7/Assignment2.java
@@ -4,7 +4,7 @@ public class Assignment2 {
     public static void main(String[] args) {
         // Find Missing Number in an array.
         // Numbers should be in range and it should not have duplicate elements.
-        int arr[] = {1,2,3,5};
+        int[] arr = {1,2,3,5};
         int current_sum = 0;
         int sum_of_range=0;
 
