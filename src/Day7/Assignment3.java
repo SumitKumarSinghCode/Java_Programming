5de2388154ab93a29afc42bd6c4e8b8b01a6f706
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-10-04 13:38:24 +0530

    update

--- src/Day7/Assignment3.java
+++ src/Day7/Assignment3.java
@@ -4,7 +4,7 @@ public class Assignment3 {
     public static void main(String[] args){
         // Find the smallest element in an array.
 
-        int arr[] = {100,200,800,500,50,9};
+        int[] arr = {100,200,800,500,50,9};
         int smallest = arr[0];
 
         for(int num:arr)
