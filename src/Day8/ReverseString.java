2d3a2b3418ccc270b594ec8a79c684421ea7115e
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-10-05 17:01:39 +0530

    update

--- src/Day8/ReverseString.java
+++ src/Day8/ReverseString.java
@@ -1,5 +1,6 @@
 package Day8;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReverseString {
@@ -17,9 +18,33 @@ public class ReverseString {
         for(int  i=str.length()-1;i>=0;i--){
             rev+=str.charAt(i);
         }
-        System.out.println("Reversed String:"+rev);
+        System.out.println("Reversed String By using charAt() method: "+rev);
+
+
 
         // Method-2 By converting the string into char array
+        System.out.println();
+        String string = "Welcome";
+        String reverse = "";
+        char[] char_array = string.toCharArray(); // this will convert string into char type of array
+        System.out.println("String into char array: "+Arrays.toString(char_array)); //[W, e, l, c, o, m, e]
+
+        for(int i=char_array.length-1; i>=0;i--)
+        {
+            reverse+=char_array[i];
+        }
+        System.out.println("Reversed array By converting the string into char array: "+reverse); //emocleW
+
+
+        // Method-3 By using builtin method(StringBuffer class)
+        System.out.println();
+        StringBuffer S = new StringBuffer("Welcome");
+        System.out.println("Reverse String using StringBuffer class: "+S.reverse());
+
+        // Method- 4 By using StringBuilder class
+        System.out.println();
+        StringBuilder s = new StringBuilder("Welcome");
+        System.out.println("Reverse String using StringBuilder class: "+s.reverse());
 
     }
 }
