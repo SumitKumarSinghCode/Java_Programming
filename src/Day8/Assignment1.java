36a04b174ddbcacdc4a91e8c93043d8a0f9ba7e2
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-10-07 13:49:36 +0530

    update

--- src/Day8/Assignment1.java
+++ src/Day8/Assignment1.java
@@ -12,9 +12,8 @@ public class Assignment1 {
 
         String rev = "";
         for (int i=s.length()-1;i>=0;i--)
-        {
-            rev+=s.charAt(i);
-        }
+            rev += s.charAt(i);
+
         if(s.equals(rev))
         {
             System.out.println("Palindrome");
