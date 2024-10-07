87b1940298e7e69626d6d6d05e4934a15321d2f7
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-10-07 13:56:36 +0530

    update

---
+++ src/Day8/Assignment3.java
@@ -0,0 +1,11 @@
+package Day8;
+
+public class Assignment3 {
+    public static void main(String[] args){
+        // Remove white space in a string
+
+        String s = "we  l co m e";
+        s = s.replace(" ","");
+        System.out.println(s); // welcome
+    }
+}
