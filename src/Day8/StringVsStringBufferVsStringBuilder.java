381a0927cb4dd68cfd8ec7cf60c859df882c89c4
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-10-07 12:58:44 +0530

    update

--- src/Day8/StringVsStringBufferVsStringBuilder.java
+++ src/Day8/StringVsStringBufferVsStringBuilder.java
@@ -10,6 +10,17 @@ public class StringVsStringBufferVsStringBuilder {
         System.out.println(s.concat(s2)); // Welcometo java
         System.out.println(s); // Welcome (when we apply any method to Sting it dosenot effect original value)
 
+        // StringBuffer - Mutable(can change)
+        System.out.println();
+        StringBuffer S = new StringBuffer("welcome");
+        S.append("to java"); // (permanent change in the String)
+        System.out.println(S); // welcometo java
+
+        // StringBuilder - mutable(can change)
+        System.out.println();
+        StringBuilder str = new StringBuilder("welcome");
+        str.append("to java"); // (permanent change in the String)
+        System.out.println(str); // welcometo java
 
     }
 }
