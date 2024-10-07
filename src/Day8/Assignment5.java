ca50114981699139519bf8d3cd0c1387b909903f
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-10-07 17:01:32 +0530

    update

---
+++ src/Day8/Assignment5.java
@@ -0,0 +1,11 @@
+package Day8;
+
+public class Assignment5 {
+    public static void main(String[] args){
+        // Count words in a string
+        String s = "Welcome to java with selenium";
+
+        String[] arr = s.split(" ");
+        System.out.println("Count of words in the string is "+arr.length);
+    }
+}
