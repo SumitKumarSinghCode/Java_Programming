c81867537d644a3d6a55b2f20f8fb523bb197f70
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-10-07 13:26:45 +0530

    update

---
+++ src/Day8/Assignment1.java
@@ -0,0 +1,28 @@
+package Day8;
+
+import java.util.Scanner;
+
+public class Assignment1 {
+    public static void main(String[] args){
+        // Check String is palindrome or not
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a String");
+        String s = scn.next();
+        System.out.println("Input String is: "+s);
+
+        String rev = "";
+        for (int i=s.length()-1;i>=0;i--)
+        {
+            rev+=s.charAt(i);
+        }
+        if(s.equals(rev))
+        {
+            System.out.println("Palindrome");
+        }
+        else
+        {
+            System.out.println("Not Palindrome");
+        }
+
+    }
+}
