40f15aacaf675e1ac995807c8dd39918af85c615
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-10-08 18:19:32 +0530

    update

---
+++ src/Day11/Test.java
@@ -0,0 +1,12 @@
+package Day11;
+
+public class Test {
+
+    int number;
+
+     void m1(int number)
+     {
+         number = number+10;
+         System.out.println("Value in the method: "+number);
+     }
+}
