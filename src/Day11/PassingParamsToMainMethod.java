aed9bb30d482089a0cc4e295e52d9279b4edbad6
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-10-09 12:24:19 +0530

    update

--- src/Day11/PassingParamsToMainMethod.java
+++ src/Day11/PassingParamsToMainMethod.java
@@ -3,6 +3,7 @@ package Day11;
 public class PassingParamsToMainMethod {
     public static void main(String[] a){
 
+        // we can only pass parameters in main method while run time
         // Printing main methods array elements
         for(String s:a){
             System.out.println(s);
