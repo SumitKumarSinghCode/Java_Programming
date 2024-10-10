a8392eb0f1427f6d5aa35f87af02fbd531d6cf79
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-10-10 16:31:49 +0530

    update

---
+++ src/Day13/Test.java
@@ -0,0 +1,13 @@
+package Day13;
+
+// when ever we create a class by default it is extended from Object class(Object class is root/parent class of all the classes in java)
+class ABC{
+
+}
+public class Test {
+    public static void main(String[] args){
+
+        ABC obj = new ABC();
+
+    }
+}
