46392af67f0f4c42eac7a6e96c65d0bdd8a4f4d2
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-10-10 16:40:16 +0530

    update

--- src/Day13/Test.java
+++ src/Day13/Test.java
@@ -1,9 +1,15 @@
 package Day13;
 
 // when ever we create a class by default it is extended from Object class(Object class is root/parent class of all the classes in java)
+// Class can not extend multiple classes because Object class is root/parent class of all the classes in java
 class ABC{
 
 }
+
+class XYZ{
+
+}
+
 public class Test {
     public static void main(String[] args){
 
