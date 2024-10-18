7536bb8ca10b763beecc1884f0a1bd1cdef24305
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-10-18 16:18:34 +0530

    update

--- src/Day15/InterfaceDemo.java
+++ src/Day15/InterfaceDemo.java
@@ -23,7 +23,7 @@ public class InterfaceDemo implements Shape
     }
 
     void triangle(){
-        System.out.println("This is triangle-- default method");
+        System.out.println("This is triangle-- Normal class method");
     }
 
     public static void main(String[] args){
@@ -32,17 +32,18 @@ public class InterfaceDemo implements Shape
         InterfaceDemo idobj = new InterfaceDemo();
         idobj.square(); // Output-(This is square-- default method) default Method (Calls the default method through class object if not overridden)
         idobj.circle(); // Abstract Method  Output-(This is circle-- Abstract method)
+        idobj.triangle(); //Output-(This is triangle-- Normal class method) Normal class Method.
         System.out.println(Shape.length+" "+ Shape.width); // Output-(10 20) Accessing (final and static) variable of Interface without any object. (This is good practice)
         System.out.println(idobj.length+" "+ idobj.width); // Output-(10 20) Accessing (final and static) variable of Interface through object. (This is bad practice)
         /*
         1) Static methods in interfaces were introduced in Java 8. These methods can have a body and are used for utility or helper methods.
         2) You cannot call a static method of an interface using an object of a class that implements the interface. You must use the interface name to call the static method.
          */
-        Shape.rectangle();
-
+        Shape.rectangle(); // Static Methods can be accessed directly through interface
 
 
         //Scenario-2
+
 
 
     }
