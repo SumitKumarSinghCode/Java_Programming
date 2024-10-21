997403837ad94afe02f79d3f4dc2a1ef06c7a1c7
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-10-21 16:33:01 +0530

    update

--- src/Day16/DataConvertions.java
+++ src/Day16/DataConvertions.java
@@ -2,6 +2,7 @@ package Day16;
 
 public class DataConvertions {
     public static void main(String[] args){
+        // Scenario -1 ( String --> int, double, bool, char(not possible))  (Unboxing)
 
         // String to int
         // String s = "Welcome"; // cannot convert to number
@@ -18,12 +19,43 @@ public class DataConvertions {
 
         // String to boolean
         String s5="TRUE"; // string can be in any case
-        boolean boolvalue = Boolean.parseBoolean(s5);
-        System.out.println(boolvalue); // true
+        boolean blvalue = Boolean.parseBoolean(s5);
+        System.out.println(blvalue); // true
 
         // String to char ( cannot convert to char because multiple character cannot be converted to single character )
         // String s ="welcome"; // cannot convert to char(Not possible)
 
 
+        // Scenario -2 (int, double, bool, char --> String) (Auto Boxing)
+        int x=100;
+        String xstring = String.valueOf(x);
+        System.out.println(xstring); // 100
+
+        double d = 10.5;
+        String dstring= String.valueOf(d);
+        System.out.println(dstring); // 10.5
+
+        boolean b = true;
+        String bstring = String.valueOf(b);
+        System.out.println(bstring); // true
+
+        char c ='A';
+        String cstring = String.valueOf(c);
+        System.out.println(c); // A
+
+        // Scenario -3 (int to double)
+        int num =10;
+        // Approach-1
+        double dbl =num;
+        // Approach-2
+        double dvalue =Double.valueOf(num);
+
+        System.out.println(dvalue+" "+dbl); // 10.0 10.0
+
+        // Scenario -4 (double to int) we don't convert because decimal point will be missing
+        double y = 10.5;
+        // int z = y; // Not possible
+         int z = (int)y; // By type casting we can do it
+        System.out.println(z); // 10
     }
 }
