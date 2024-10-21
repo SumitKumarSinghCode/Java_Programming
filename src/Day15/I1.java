19318581b8845e4bbb7b6cbf82f072daf38e7f86
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-10-21 12:00:05 +0530

    update

--- src/Day15/I1.java
+++ src/Day15/I1.java
@@ -1,7 +1,8 @@
 package Day15;
 
 public interface I1 {
-
+    // In interface the methods are default and static
+    // We can only create an Abstract class inside the interface
     int x=100;
     void m1();
 }
--- src/Day15/I2.java
+++ src/Day15/I2.java
@@ -1,7 +1,8 @@
 package Day15;
 
 public interface I2 {
-
+    // In interface the methods are default and static
+    // We can only create an Abstract class inside the interface
     int y=200;
     void m2();
 
